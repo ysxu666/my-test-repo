0fb3ac3be96212645a09c58a7d15d2c32bf5d3a5
xys <xys@192.168.0.100>
2025-05-18 19:29:15 +0800

    Your commit message2

--- calculator.py
+++ calculator.py
@@ -3,9 +3,9 @@ class Calculator:
         """Return the sum of a and b."""
         return a + b
 
-    def subtract(self, a, b):
-        """Return the difference of a and b."""
-        return a - b
+    # def subtract(self, a, b):
+    #     """Return the difference of a and b."""
+    #     return a - b
 
     # def multiply(self, a, b):
     #     """Return the product of a and b."""
